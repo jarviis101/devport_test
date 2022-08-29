88504f973ef7c3f8390d29305daa93f0d08482ee
Aleksandr Kharchenko <dnc.oleksandr.kharchenko@gmail.com>
2022-08-29 10:40:40 +0300

    draft for calculator

--- app/service/calculator.py
+++ app/service/calculator.py
@@ -7,11 +7,13 @@ class IndexCalculator:
 
     def calculate(self, dto: CalculatorDTO):
         index = self.MIN_INDEX_VALUE
-        first_team_set = list(dto.get_first_team())
-        second_team_set = list(dto.get_second_team())
+        first_team_list = list(dto.get_first_team())
+        second_team_list = list(dto.get_second_team())
 
-        for first_team_letter in first_team_set:
-            for second_team_letter in second_team_set:
-                if first_team_letter == second_team_letter:
-                    pass
+        if first_team_list == second_team_list:
+            return self.MAX_INDEX_VALUE
+
+        if len(first_team_list) != len(second_team_list):
+            # TODO check if length`s not equals
+            return index
         return index
