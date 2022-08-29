433d2339e83ce13ceb2789db68ca6d164b9bf965
Aleksandr Kharchenko <dnc.oleksandr.kharchenko@gmail.com>
2022-08-29 13:00:28 +0300

    draft for calculator

--- app/service/calculator.py
+++ app/service/calculator.py
@@ -6,14 +6,22 @@ class IndexCalculator:
     MAX_INDEX_VALUE = 1.00
 
     def calculate(self, dto: CalculatorDTO):
-        index = self.MIN_INDEX_VALUE
-        first_team_list = list(dto.get_first_team())
-        second_team_list = list(dto.get_second_team())
+        first_team_list = list(
+            dto.get_first_team().replace(" ", "").lower()
+        )
+        second_team_list = list(
+            dto.get_second_team().replace(" ", "").lower()
+        )
 
         if first_team_list == second_team_list:
             return self.MAX_INDEX_VALUE
 
-        if len(first_team_list) != len(second_team_list):
-            # TODO check if length`s not equals
-            return index
-        return index
+        count = 0
+        for i in range(min(len(first_team_list), len(second_team_list))):
+            if first_team_list[i] == second_team_list[i]:
+                count += 1
+
+        if count > 0:
+            idx = self.MAX_INDEX_VALUE - (self.MAX_INDEX_VALUE / count)
+            return idx
+        return self.MIN_INDEX_VALUE
